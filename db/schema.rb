5a8b916e8afe8479d3bc2139fc4a3b461c6c7685
Fisenko V <vyacheslav.fisenko@gmail.com>
2016-08-26 01:09:21 +0300

    6.3

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825105929) do
+ActiveRecord::Schema.define(version: 20160825220421) do
 
   create_table "lamers", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_lamers_on_email", unique: true
   end
 
   create_table "users", force: :cascade do |t|
